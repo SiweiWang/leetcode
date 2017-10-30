507e3f6fed301c06144c62706034774b76008f9d
Siwei Wang <Siwei.Wang@scotiabank.com>
2017-10-29 21:30:19 -0400

    numer of locks

--- src/NumberOfLocks.java
+++ src/NumberOfLocks.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
 public class NumberOfLocks {
+    public boolean [] numbeOfLocks(int count){
+        boolean [] locks = new boolean [count];
+        for (int i = 0; i < locks.length; i ++){
+            // true means locked
+            locks[i] = true;
+        }
+        for (int i = 1; i <= count; i ++){
+            for (int j =1 ; j <= locks.length; j ++){
+                if (j % i ==0){
+                    locks[j-1] = !locks[j-1];
+                }
+            }
+        }
+        return locks;
+    }
+
+    public static void main(String[] args){
+        NumberOfLocks n = new NumberOfLocks();
+        boolean [] s  = n.numbeOfLocks(100);
+        int count = 0;
+        for (int i = 0; i < s.length; i ++){
+            if (s[i]){
+                count ++;
+                System.out.println(i+1);
+            }
+        }
+
+        System.out.println("Total: " + count);
+    }
 }
