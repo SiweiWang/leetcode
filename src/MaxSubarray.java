863522871abeb94fb1e02c04870e34096ca19b62
Siwei Wang <Siwei.Wang@scotiabank.com>
2017-10-29 21:29:41 -0400

    Max subarray

--- src/MaxSubarray.java
+++ src/MaxSubarray.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class MaxSubarray {
+    public int maxSubArray(int[] nums) {
+        int result = Integer.MIN_VALUE, sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum = Math.max(sum + nums[i], nums[i]);
+            result = Math.max(result, sum);
+        }
+        return result;
+    }
 }
