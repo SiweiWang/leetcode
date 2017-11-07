301a80126c0165194fadd9492fd7bc6dfb57b1ea
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-06 21:41:12 -0500

    house robber

--- src/HouseRobber.java
+++ src/HouseRobber.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class HouseRobber {
+    public int rob(int [] nums){
+
+        int a = 0, b = 0;
+        for (int i = 0; i < nums.length; i ++){
+            if( i % 2 == 0){
+                a += nums[i];
+                System.out.println("num[i] = " + nums[i]  + " a = " + a + " b = " + b);
+
+                a = Math.max(a, b);
+            }else {
+                b += nums[i];
+                System.out.println("num[i] = " + nums[i]  + " a = " + a + " b = " + b);
+
+                b = Math.max(a, b);
+            }
+        }
+
+        return Math.max(a, b);
+    }
+
+    public static void main(String[] args){
+        HouseRobber h = new HouseRobber();
+        h.rob(new int [] {1, 2, 100, 4,5, 6});
+    }
 }
