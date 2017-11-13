ed91601bf2e0beea138d57d5c2c3429c97bbaa11
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-12 23:40:52 -0500

    rotate array

--- src/RotateArray.java
+++ src/RotateArray.java
@@ -1,7 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 /**
  * Created by siweiwang on 2017-11-05.
  */
 public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        int [] a = new int [nums.length];
+        for (int i = 0; i < nums.length; i ++ ){
+            a[(i+k) % nums.length] = nums[i];
+        }
+
+        for (int i = 0; i < nums.length; i ++){
+            nums[i] = a[i];
+        }
+    }
+
+
+
 }
