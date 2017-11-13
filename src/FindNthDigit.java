211d7d9ae35f4ebc1e2f4e1431bd3110cff3f68b
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-12 23:38:04 -0500

    find Nth Digit

--- src/FindNthDigit.java
+++ src/FindNthDigit.java
@@ -1,7 +1,21 @@
-package PACKAGE_NAME;
-
 /**
  * Created by siweiwang on 2017-11-08.
  */
 public class FindNthDigit {
+    public int findNthDigit(int n){
+        int len =1;
+        long count = 9;
+        int start = 1;
+
+        while (n > len * count ){
+            n -= len *count;
+            len += 1;
+            count *= 10;
+            start *= 10;
+        }
+
+        start += (n -1) / len;
+        String s = Integer.toString(start);
+        return Character.getNumericValue(s.charAt((n-1) % len));
+    }
 }
