8ab53a9caa404bca71e4bd81110a366116f61d70
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-12 23:39:53 -0500

    min stack

--- src/MinStack.java
+++ src/MinStack.java
@@ -1,7 +1,35 @@
-package PACKAGE_NAME;
-
+import java.util.Stack;
 /**
  * Created by siweiwang on 2017-11-02.
  */
-public class MinStack {
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> min;
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<Integer>();
+        min = new Stack<Integer>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (min.empty() || min.peek() >= x){
+            min.push(x);
+        }
+    }
+
+    public void pop() {
+        if (this.top() == this.getMin()){
+            min.pop();
+        }
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min.peek();
+    }
 }
