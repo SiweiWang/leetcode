62b78e04da25662dd5457e61c6b8fec61f65839c
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-12 23:39:34 -0500

    Is Isomorphic

--- src/IsIsomorphic.java
+++ src/IsIsomorphic.java
@@ -1,7 +1,18 @@
-package PACKAGE_NAME;
-
 /**
  * Created by siweiwang on 2017-11-08.
  */
 public class IsIsomorphic {
+    public boolean isIsomorphic(String s, String t) {
+        int [] s_list = new int [256];
+        int [] t_list = new int [256];
+        for (int i =0; i < s.length(); i ++){
+            if (s_list[(int)(s.charAt(i))] != t_list[(int)(t.charAt(i))]){
+                return false;
+            }else{
+                s_list[(int)(s.charAt(i))] = i + 1;
+                t_list[(int)(t.charAt(i))] = i + 1;
+            }
+        }
+        return true;
+    }
 }
