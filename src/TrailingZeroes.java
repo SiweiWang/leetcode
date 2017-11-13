e10b36a53409be3da8ba37f2c4ac3250e68e53fd
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-12 23:41:26 -0500

    Trailing Zeroes

--- src/TrailingZeroes.java
+++ src/TrailingZeroes.java
@@ -1,7 +1,13 @@
-package PACKAGE_NAME;
-
 /**
  * Created by siweiwang on 2017-11-05.
  */
 public class TrailingZeroes {
+    public int trailingZeroes(int n) {
+        int result = 0;
+        while (n > 0){
+            result += n/5;
+            n = n/5;
+        }
+        return result;
+    }
 }
