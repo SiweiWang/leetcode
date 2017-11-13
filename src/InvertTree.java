6b78c00a8a31e2909c93ba0fb900a1f0721b4b2d
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-12 23:39:04 -0500

    invert tree

--- src/InvertTree.java
+++ src/InvertTree.java
@@ -1,7 +1,12 @@
-package PACKAGE_NAME;
-
 /**
  * Created by siweiwang on 2017-11-08.
  */
 public class InvertTree {
+    public TreeNode invertTree(TreeNode root){
+        if (root == null) { return root;}
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+        return root;
+    }
 }
