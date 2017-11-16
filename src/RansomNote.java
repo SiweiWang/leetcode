045948e2a613a693a24eaced0f3383c33a3b4ea8
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-15 23:37:05 -0500

    ransom note

--- src/RansomNote.java
+++ src/RansomNote.java
@@ -1,7 +1,34 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
 
 /**
  * Created by siweiwang on 2017-11-14.
  */
 public class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        char [] mag_list = magazine.toCharArray();
+        for (char c: mag_list){
+            if (map.containsKey(c)){
+                map.put(c, map.get(c) +1);
+            }else{
+                map.put(c, 1);
+            }
+        }
+
+        char [] ransom_list = ransomNote.toCharArray();
+        for (char c : ransom_list){
+            if (map.containsKey(c) && map.get(c) > 0){
+                map.put(c, map.get(c) - 1);
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String [] args){
+        RansomNote r = new RansomNote();
+        System.out.println(r.canConstruct("aa", "aab"));
+    }
+
 }
