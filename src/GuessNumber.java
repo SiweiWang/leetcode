1dd6b1079bda3eef4fd4b1559d2c68ce598f2ffa
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-15 23:34:01 -0500

    guess number

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -1,7 +1,26 @@
-package PACKAGE_NAME;
-
 /**
  * Created by siweiwang on 2017-11-13.
  */
 public class GuessNumber {
+    public int guessNumber(int n) {
+        if (guess(n) == 0) return n;
+        int start = 1;
+        int last = n;
+        while (start < last ){
+            int mid = start + (last - start) /2;
+            int t = guess(mid);
+            if (t ==0 ){
+                return mid;
+            }else  if (t ==1 ) {
+                start = mid;
+            } else {
+                last = mid;
+            }
+        }
+        return start;
+    }
+
+    public int guess(int n){
+        return -1;
+    }
 }
