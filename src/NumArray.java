9884286144451bd999cbd2de7dfaf7d0b813806e
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-15 23:36:38 -0500

    numer array

--- src/NumArray.java
+++ src/NumArray.java
@@ -1,7 +1,16 @@
-package PACKAGE_NAME;
-
 /**
  * Created by siweiwang on 2017-11-13.
  */
 public class NumArray {
+    int [] dp;
+    public NumArray(int[] nums) {
+        dp = new int [nums.length];
+        for (int i = 0; i < nums.length; i++){
+            dp[i] = i == 0 ? nums[i] : nums[i] + dp[i -1];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return i == 0 ? dp[j] : dp[j] - dp[i-1];
+    }
 }
