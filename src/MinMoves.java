d3f9184013cd920035e08b23b81f8ea62aa64916
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-19 23:33:53 -0500

    min moves

--- src/MinMoves.java
+++ src/MinMoves.java
@@ -1,7 +1,17 @@
-package PACKAGE_NAME;
-
 /**
  * Created by siweiwang on 2017-11-19.
  */
 public class MinMoves {
+    public int minMoves(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int result = 0;
+        for (int i : nums) {
+            min = Math.min(min, i);
+        }
+        for (int i : nums) {
+            result += i - min;
+        }
+
+        return result;
+    }
 }
