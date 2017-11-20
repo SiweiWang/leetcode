9802546f40f099757e674cad9d7b6df2a720adcf
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-19 23:34:15 -0500

    src/PoorPigs.java

--- src/PoorPigs.java
+++ src/PoorPigs.java
@@ -1,7 +1,9 @@
-package PACKAGE_NAME;
-
 /**
  * Created by siweiwang on 2017-11-19.
  */
 public class PoorPigs {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        double states = minutesToTest / minutesToDie + 1;
+        return (int)(Math.ceil(Math.log((double)buckets)/Math.log(states)));
+    }
 }
