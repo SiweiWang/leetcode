bc6032ab23936ffaa0c064f35dd2833bc3a54da2
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-19 23:32:56 -0500

    find disappaearedNumbers:

--- src/FindDisappearedNumbers.java
+++ src/FindDisappearedNumbers.java
@@ -1,7 +1,23 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by siweiwang on 2017-11-19.
  */
 public class FindDisappearedNumbers {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int [] list = new int[nums.length];
+        for (int i: nums){
+            list[i-1] += 1;
+        }
+        Integer.MIN_VALUE
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int i = 0; i < list.length; i ++){
+            if (list[i] == 0){
+                res.add(i+1);
+            }
+        }
+
+        return res;
+    }
 }
