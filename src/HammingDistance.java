2d05cb904896e30adcf5c0180dabf7f0eccfef73
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-19 23:33:21 -0500

    add hamming distance

--- src/HammingDistance.java
+++ src/HammingDistance.java
@@ -1,7 +1,19 @@
-package PACKAGE_NAME;
-
 /**
  * Created by siweiwang on 2017-11-19.
  */
 public class HammingDistance {
+    public int hammingDistance(int x, int y) {
+        int max = Math.max(x, y);
+        int res = 0;
+        while (max > 0){
+            if (x % 2 != y % 2){
+                res += 1;
+            }
+
+            x = x >> 1;
+            y = y >> 1;
+            max = max >> 1;
+        }
+        return res;
+    }
 }
