767d6cb34978df2c60aa177f58756a374e99d6f3
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-20 23:26:33 -0500

    reverse word

--- src/ReverseWords.java
+++ src/ReverseWords.java
@@ -1,7 +1,28 @@
-package PACKAGE_NAME;
-
 /**
  * Created by siweiwang on 2017-11-20.
  */
 public class ReverseWords {
+    public String reverseWords(String s) {
+        String [] s_list = s.split("\\s+");
+        for (int i =0; i < s_list.length; i ++){
+            s_list[i] = reverse(s_list[i]);
+        }
+
+        return String.join(" ", s_list);
+    }
+
+    String reverse(String s){
+        char [] list = s.toCharArray();
+        int i = 0;
+        int j = list.length -1;
+        while(i < j){
+            char temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+            i ++;
+            j --;
+        }
+        return new String(list);
+
+    }
 }
