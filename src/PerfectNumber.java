401c0b567ec3f2f79a30a2da5c82d555d66a3853
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-21 21:54:58 -0500

    Perfect number

--- src/PerfectNumber.java
+++ src/PerfectNumber.java
@@ -1,7 +1,12 @@
-package PACKAGE_NAME;
-
 /**
  * Created by siweiwang on 2017-11-21.
  */
 public class PerfectNumber {
+    public boolean checkPerfectNumber(int num) {
+        if (num == 6 || num == 8 || num == 28 || num == 496 || num == 8128 || num == 33550336){
+            return true;
+        }
+
+        return false;
+    }
 }
