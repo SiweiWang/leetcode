9733c6cd2caea4df1deb5d15fe9b93fe1ae42364
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-21 21:54:38 -0500

    find LUS length

--- src/FindLUSlength.java
+++ src/FindLUSlength.java
@@ -1,7 +1,8 @@
-package PACKAGE_NAME;
-
 /**
  * Created by siweiwang on 2017-11-21.
  */
 public class FindLUSlength {
+    public int findLUSlength(String a, String b) {
+        return (a.equals(b)) ? -1: Math.max(a.length(), b.length());
+    }
 }
