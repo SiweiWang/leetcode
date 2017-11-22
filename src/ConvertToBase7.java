328e86573ed2627ed4d768778c8553f7cc2278a6
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-21 21:54:13 -0500

    convert to base 7

--- src/ConvertToBase7.java
+++ src/ConvertToBase7.java
@@ -1,7 +1,21 @@
-package PACKAGE_NAME;
-
 /**
  * Created by siweiwang on 2017-11-21.
  */
 public class ConvertToBase7 {
+    public String convertToBase7(int num) {
+        boolean neg = false;
+        if (num < 0 ){
+            num = -num;
+            neg = true;
+        }
+        if (num == 0){
+            return "0";
+        }
+        String res = "";
+        while (num > 0){
+            res = String.valueOf( num % 7 )  + res ;
+            num = num /7;
+        }
+        return neg? "-" + res: res;
+    }
 }
