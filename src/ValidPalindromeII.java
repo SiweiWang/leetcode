649b3714b13558dac61b9899bbd388bad7476102
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-23 21:45:35 -0500

    valid palindrome

--- src/ValidPalindromeII.java
+++ src/ValidPalindromeII.java
@@ -1,7 +1,27 @@
-package PACKAGE_NAME;
-
 /**
  * Created by siweiwang on 2017-11-23.
  */
 public class ValidPalindromeII {
+    public boolean validPalindrome(String s) {
+        return validPalindrome(s, false);
+    }
+
+    boolean validPalindrome(String s, boolean found){
+        char[] list = s.toCharArray();
+        int start = 0;
+        int end = s.length() -1 ;
+        while(start < end){
+            if(list[start] != list[end]){
+                if (found){
+                    return false;
+                }else{
+                    return validPalindrome(s.substring(start+1, end+1), true) || validPalindrome(s.substring(start, end), true);
+                }
+            }else{
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
 }
