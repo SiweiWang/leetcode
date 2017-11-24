3bb3cb7e3495ff9104fa021b284246b7b1d2cf39
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-23 21:42:55 -0500

    repeated string match

--- src/RepeatedStringMatch.java
+++ src/RepeatedStringMatch.java
@@ -1,7 +1,18 @@
-package PACKAGE_NAME;
-
 /**
  * Created by siweiwang on 2017-11-23.
  */
 public class RepeatedStringMatch {
+    public int repeatedStringMatch(String A, String B) {
+        String t = A;
+        int count = 1;
+        while(t.length() < B.length()){
+            t = t + A;
+            ++ count;
+        }
+        if (t.indexOf(B) >= 0){
+            return count;
+        }
+        t = t + A;
+        return t.indexOf(B) >=0 ? ++count: -1;
+    }
 }
