e5af260d9e4ea19d3cacfac05b0ba91d0ff90fda
Siwei Wang <wangsiwei2003@hotmail.com>
2017-11-23 21:42:13 -0500

    has alternative bits

--- src/HasAlternatingBits.java
+++ src/HasAlternatingBits.java
@@ -1,7 +1,18 @@
-package PACKAGE_NAME;
-
 /**
  * Created by siweiwang on 2017-11-23.
  */
 public class HasAlternatingBits {
+    public boolean hasAlternatingBits(int n) {
+        int start = n %2;
+        n = n >>1;
+        while(n > 0){
+            if (n % 2 == start){
+                return false;
+            }
+            start = n %2;
+            n = n >>1;
+        }
+
+        return true;
+    }
 }
