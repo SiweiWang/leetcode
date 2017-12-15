c47fcfe4aa5055ed15ff545ae3985ee7bfe96264
Siwei Wang <wangsiwei2003@hotmail.com>
2017-12-14 20:21:27 -0500

    trapping water

--- src/Trap.java
+++ src/Trap.java
@@ -1,7 +1,26 @@
-package PACKAGE_NAME;
-
 /**
  * Created by siweiwang on 2017-12-13.
  */
+
+import java.util.Stack;
+
 public class Trap {
+    public int trap(int[] height) {
+        Stack<Integer> s = new Stack<Integer>();
+        int i = 0;
+        int res = 0;
+        int n = height.length;
+        while(i < n){
+            if (s.empty()|| height[i] <= height[s.peek()] ){
+                s.push(i++);
+            }else{
+                int t = s.pop();
+                if(s.empty()){
+                    continue;
+                }
+                res += (Math.min(height[i], height[s.peek()]) - height[t] * (i - s.peek() -1));
+            }
+        }
+        return res;
+    }
 }
