8f7756d9f8f94c8605f16ef0b9ef38bc889664aa
Siwei Wang <siwei.wang@capco.com>
2020-01-08 20:54:24 -0500

    minimum Path Sum

---
+++ dp/minimumPathSum.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m = len(grid)
+        n = len(grid[0])
+
+        dp = [ [0 for i in range(n)] for i in range(m) ]
+
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[i][j] = grid[i][j]
+                elif i == 0:
+                    dp[i][j] = dp[i][j-1] + grid[i][j]
+                elif j == 0:
+                    dp[i][j] = dp[i-1][j] + grid[i][j]
+                else:
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+
+        return dp[m-1][n-1]
+
+def main():
+    sol = Solution()
+    print(sol.minPathSum([[1,3,1],[1,5,1],[4,2,1]]))
+
+
+if __name__ == "__main__":
+    main()
